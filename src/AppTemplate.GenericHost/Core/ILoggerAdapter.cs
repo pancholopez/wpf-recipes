2bab243cd2e79c83e1027816b7d4883e354f1f5e
Francisco Lopez <francisco.lopez2@thermofisher.com>
2020-03-06 15:18:23 +0100

    added logger adapter interface

---
+++ src/AppTemplate.GenericHost/Core/ILoggerAdapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppTemplate.GenericHost.Core
+{
+    public interface ILoggerAdapter
+    {
+        void Info(string message);
+        void Warning(string message);
+        void Error(string message);
+        void Error(Exception exception, string message);
+    }
+}
